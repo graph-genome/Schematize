c2a996d0cfe8faeb8ab70d17bf9910711de47264
Josiah Seaman <josiah@newline.us>
2019-11-26 11:00:56 +0100

    Major: experimenting with LinkColumn phase display.  Ran into serious performance issues.

--- src/LinkRect.js
+++ src/LinkRect.js
@@ -1,44 +1,50 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
+import React from 'react';
+import {Rect} from 'react-konva';
 
 class LinkRect extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         // this.updateCanvas = this.updateCanvas.bind(this);
         let index = 0;
-        let coloredList = [];
 
-        // Create colored list for putting dots on Link rectangle.
-/*
-        props.item.participants.forEach((item, i) => {
-            while(item !== props.pathNames[index]) {
-                index += 1;
-                coloredList.push(false)
-            }
-            coloredList.push(true)
-            index += 1
-        })
-        while(index < props.pathNames.length) {
-            index += 1
-            coloredList.push(false)
-        }
-*/
-        this.state = {
-            coloredList: coloredList,
-            color: this.props.color
-        } 
-    };
+
+    };/*
     handleClick = () => {
         this.setState({
             color: Konva.Util.getRandomColor()
         });
-    };
-    /*
-    componentDidMount() {
-        this.updateCanvas();
+    };*/
+
+    dots() {
+        return this.state.coloredList.map((boolean, i) => {
+            if (boolean) {
+                return (
+                    <Rect
+                        x={this.props.x}
+                        y={this.props.y + i}
+                        width={this.props.width}
+                        height={1}
+                        fill={this.props.color}
+                        // onClick={this.handleClick}
+                    />)
+            }
+        })
+
     }
+    /**/
+    componentDidMount() {
+        // Create colored list for putting dots on Link rectangle.
+        let coloredList = [];
+        /** // CONCLUSION: Colored list dots for link column must be calculated in Python segmentation.
+        this.props.pathNames.forEach((p_name, i) => {
+            coloredList.push(this.props.item.participants.contains(p_name));
+        });
+        /**/
+        this.setState({
+            coloredList: coloredList,
+            color: this.props.color
+        } );
+    }/** /
     componentDidUpdate() {
         this.updateCanvas();
     }
@@ -46,29 +52,25 @@ class LinkRect extends React.Component {
     updateCanvas() {
         const ctx = this.refs.rect;
         ctx.fillRect(0, 0, 100, 100);
-    }*/
-                    /*/
-            {this.state.coloredList.map((boolean, i) => {if (boolean) { return(
+    }/**/
+    render() {
+            /** /
+        return (
+            <div>
+                {this.dots()}
+            </div>
+            );
+            /*/
+        return (
             <Rect
                 x={this.props.x}
-                y={this.props.y + i}
+                y={this.props.y}
                 width={this.props.width}
-                height={1}
-                fill={this.state.color}
-                onClick={this.handleClick}
-            />)
-            }})}
-            /*/
-    render() {
-        return (
-                <Rect
-                    x={this.props.x}
-                    y={this.props.y}
-                    width={this.props.width}
-                    height={this.props.item.participants.length * this.props.pathsPerPixel}
-                    fill={this.state.color}
-                />
+                height={this.props.item.participants.length * this.props.pathsPerPixel}
+                fill={this.props.color}
+            />
         );
+        /**/
     }
 }
 export default LinkRect;
--- src/graphComponent.js
+++ src/graphComponent.js
@@ -1,13 +1,17 @@
-let fs = require('fs')
+// import set from "set";
+//var Set = require('set');
+let fs = require('fs');
 
 
 class PangenomeSchematic {
 	constructor(fileName) {
 		this.jsonPath = fileName;
 	}
-	readFile(jsonPath) {
-		console.log(jsonPath);
-		var jsonFile = require('./data/Athaliana.bin100000.schematic.json')
+	readFile() {
+		// console.log();
+		// var jsonFile = require('./data/Athaliana.bin100000.schematic.json'); // This cannot be a variable
+		var jsonFile = require('./data/yeast_bin10k_7indiv_16chr.schematic.json'); // This cannot be a variable
+
 		console.log(jsonFile)
 		return(jsonFile)
 	}
@@ -18,10 +22,11 @@ class PangenomeSchematic {
 			var componentItem = new Component(jsonFile.components[component], offsetLength)
 			offsetLength += componentItem.arrivals.length + componentItem.departures.length;
 			componentArray.push(componentItem);
+			//if(component > 20){break} // debugging: to limit input data
 		}
 		this.components = componentArray;
 		this.pathNames = jsonFile.path_names;
-		//return (componentArray)
+		return (componentArray)
 	}
 }
 
@@ -30,7 +35,7 @@ class LinkColumn {
 	constructor(linkColumn) {
 		this.upstream = linkColumn.upstream;
 		this.downstream = linkColumn.downstream;
-		this.participants = linkColumn.participants;
+		this.participants = (linkColumn.participants);//new Set
 	}
 }
 
@@ -40,16 +45,20 @@ class Component {
 		this.offset = offsetLength;
 		this.firstBin = component.first_bin;
 		this.lastBin = component.last_bin;
-		this.arrivals = []
-		for (var arrival in component.arrivals) {this.arrivals.push(new LinkColumn(component.arrivals[arrival]))};
-		this.departures = []
-		for (var departure in component.departures) {this.departures.push(new LinkColumn(component.departures[departure]))};
+		this.arrivals = [];
+		for (var arrival in component.arrivals) {
+			this.arrivals.push(new LinkColumn(component.arrivals[arrival]))
+		}
+		this.departures = [];
+		for (var departure in component.departures) {
+			this.departures.push(new LinkColumn(component.departures[departure]))
+		}
 		
 	}
 }
 
-var schematic = new PangenomeSchematic()
+var schematic = new PangenomeSchematic();
 
-var jsonFile = schematic.readFile('./data/Athaliana.bin100000.schematic.json')
-schematic.processArray(jsonFile)
+var jsonFile = schematic.readFile();
+schematic.processArray(jsonFile);
 export default schematic
