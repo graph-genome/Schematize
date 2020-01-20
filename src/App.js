fa7d48701dfb21aa18680f224eb1fe1f794103f2
Josiah Seaman <josiah@newline.us>
2020-01-20 10:12:45 -0700

    Major refactor to clean up render().  No logical changes.

--- src/App.js
+++ src/App.js
@@ -47,8 +47,10 @@ class App extends Component {
     let binsPerPixel = 4;
     let paddingSize = 2;
     let leftOffset = 10;
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    let actualWidth = leftOffset + schematic.components.map(component => component.arrivals.length + component.departures.length + (component.lastBin - component.firstBin) + 1 + paddingSize).reduce(reducer) * binsPerPixel;
+    const sum = (accumulator, currentValue) => accumulator + currentValue;
+    let actualWidth = leftOffset + schematic.components.map(component =>
+        component.arrivals.length + component.departures.length + (component.lastBin - component.firstBin) + 1 + paddingSize
+    ).reduce(sum) * binsPerPixel;
     console.log(actualWidth);
     this.state = { 
       schematize: schematic.components, 
@@ -101,146 +103,122 @@ class App extends Component {
       this.setState({highlightedLinkId: linkRect})
   };
 
+
+  renderComponent(schematizeComponent, i) {
+    return (
+        <React.Fragment>
+          <ComponentRect
+              item={schematizeComponent}
+              key={i}
+              x={this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset) * this.state.binsPerPixel}
+              y={this.state.topOffset}
+              height={this.state.pathNames.length * this.state.pathsPerPixel}
+              width={((schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + schematizeComponent.arrivals.length + schematizeComponent.departures.length) * this.state.binsPerPixel}
+          />
+
+          {schematizeComponent.arrivals.map(
+              (linkColumn, j) => {
+                let leftPadding = 0;
+                return this.renderLinkColumn(schematizeComponent, i, leftPadding, j, linkColumn);
+              }
+          )}
+          {schematizeComponent.departures.map(
+              (linkColumn, j) => {
+                let leftPadding = (schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + schematizeComponent.arrivals.length;
+                return this.renderLinkColumn(schematizeComponent, i, leftPadding, j, linkColumn);
+              }
+          )}
+        </React.Fragment>
+    )
+  }
+
+  renderLinkColumn(schematizeComponent, i, leftPadding, j, linkColumn) {
+    let xCoordArrival = this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset + leftPadding + j) * this.state.binsPerPixel;
+    let localColor = stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId);
+    return <LinkRect
+        key={"departure" + i + j}
+        item={linkColumn}
+        pathNames={this.state.pathNames}
+        x={xCoordArrival}
+        pathsPerPixel={this.state.pathsPerPixel}
+        y={this.state.topOffset}
+        width={this.state.binsPerPixel}
+        color={localColor}
+        updateHighlightedNode={this.updateHighlightedNode}
+    />
+  }
+
+  allArrivalsForOneComponent(schematizeComponent, i) {
+    return (
+        <React.Fragment>
+          {schematizeComponent.arrivals.map((linkColumn, j) => {
+            const elevation = -15 - (j * this.state.binsPerPixel);
+            const xOffsetGrid = (linkColumn.downstream + (i * this.state.paddingSize) + schematizeComponent.offset +
+                (schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + j) - .75;
+            // const departureX = departure.offset + departure.arrivals.length;
+            const localColor = stringToColourSave((linkColumn.downstream + 1) * (linkColumn.upstream + 1));
+            let departureX = colToCoordMappingX[localColor][1];
+
+            const arrowXCoord = 1 + this.state.leftOffset + xOffsetGrid * this.state.binsPerPixel;
+
+            departureX = departureX - arrowXCoord + 2;
+
+            const departOrigin = [departureX, 0];
+            const departCorner = [departureX - 2, elevation + 2];
+            let departTop = [departureX - 10, elevation];
+            let arriveTop = [10, elevation];
+            let arriveCorner = [1.5, elevation + 1.5]; // 1.5 in from actual corner
+            const arriveCornerEnd = [0, -5];
+            // we ran into a minimal sized arrow
+            if (departureX <= 12) {
+              departTop = [0, elevation + 2];
+              arriveTop = [10 - 10 + 4, elevation + 2];
+              arriveCorner = [0, elevation + 1.5]; // 1.5 in from actual corner
+            }
+            return <ArrowRect
+                key={"arrow" + i + j}
+                x={arrowXCoord}
+                y={this.state.topOffset - 5}
+                points={[
+                  departOrigin[0], departOrigin[1],
+                  departCorner[0], departCorner[1],
+                  departTop[0], departTop[1],
+                  arriveTop[0], arriveTop[1],
+                  arriveCorner[0], arriveCorner[1],
+                  arriveCornerEnd[0], arriveCornerEnd[1],
+                  0, 0]}
+                width={this.state.binsPerPixel}
+                color={stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId)}
+            />
+          })}
+        </React.Fragment>
+    )
+  }
+
   render() {
     return (
       <React.Fragment>
-        <Stage width={this.state.actualWidth + 20} height={this.state.topOffset + this.state.pathNames.length * this.state.pathsPerPixel}>
+        <Stage
+            width={this.state.actualWidth + 20}
+            height={this.state.topOffset + this.state.pathNames.length * this.state.pathsPerPixel}>
           <Layer ref={this.layerRef}>
-            {this.state.schematize.map((schematizeComponent, i)=> {
-              return (
-                  <React.Fragment>
-                    <ComponentRect
-                        item={schematizeComponent}
-                        key={i}
-                        x={this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset) * this.state.binsPerPixel}
-                        y={this.state.topOffset}
-                        height={this.state.pathNames.length * this.state.pathsPerPixel}
-                        width={((schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + schematizeComponent.arrivals.length + schematizeComponent.departures.length) * this.state.binsPerPixel}
-                        /*numPoints={5}
-                        innerRadius={20}
-                        outerRadius={40}
-                        fill="#89b717"
-                        opacity={0.8}
-                        draggable
-                        rotation={Math.random() * 180}
-                        shadowColor="black"
-                        shadowBlur={10}
-                        shadowOpacity={0.6}
-                        onDragStart={this.handleDragStart}
-                        onDragEnd={this.handleDragEnd}*/
-                    />
-{/*                    {schematizeComponent.arrivals.map((linkColumn, j) =>
-                        <LinkRect
-                            key={"arrival" + i + j}
-                            item={linkColumn}
-                            pathNames={this.state.pathNames}
-                            x={this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset + j) * this.state.binsPerPixel}
-                            pathsPerPixel={this.state.pathsPerPixel}
-                            y={this.state.topOffset}
-                            width={this.state.binsPerPixel}
-                            number={(linkColumn.downstream + 1) * (linkColumn.upstream + 1)}
-                            color={stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId)}
-                            updateHighlightedNode={this.updateHighlightedNode}
-                        />
-                    )}*/}
-                      {schematizeComponent.arrivals.map((linkColumn, j) => {
-                        let xCoordArrival = this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset + j) * this.state.binsPerPixel;
-                        let localColor = stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId);
-                          return <LinkRect
-                              key={"arrival" + i + j}
-                              item={linkColumn}
-                              pathNames={this.state.pathNames}
-                              x={xCoordArrival}
-                              pathsPerPixel={this.state.pathsPerPixel}
-                              y={this.state.topOffset}
-                              width={this.state.binsPerPixel}
-                              number={(linkColumn.downstream + 1) * (linkColumn.upstream + 1)}
-                              color={localColor}
-                              updateHighlightedNode={this.updateHighlightedNode}
-                          />
-                          }
-                      )}
-                    {/*{schematizeComponent.departures.map((linkColumn, j) =>
-                        <LinkRect
-                            key={"departure" + i + j}
-                            item={linkColumn}
-                            pathNames={this.state.pathNames}
-                            x={this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset + (schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + schematizeComponent.arrivals.length+j)*this.state.binsPerPixel}
-                            pathsPerPixel={this.state.pathsPerPixel}
-                            y={this.state.topOffset}
-                            width={this.state.binsPerPixel}
-                            color={stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId)}
-                            updateHighlightedNode={this.updateHighlightedNode}
-                        />
-                    )}*/}
-                      {schematizeComponent.departures.map((linkColumn, j) => {
-                          let localColor = stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId);
-                            return  <LinkRect
-                                  key={"departure" + i + j}
-                                  item={linkColumn}
-                                  pathNames={this.state.pathNames}
-                                  x={this.state.leftOffset + (schematizeComponent.firstBin + (i * this.state.paddingSize) + schematizeComponent.offset + (schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + schematizeComponent.arrivals.length+j)*this.state.binsPerPixel}
-                                  pathsPerPixel={this.state.pathsPerPixel}
-                                  y={this.state.topOffset}
-                                  width={this.state.binsPerPixel}
-                                  color={localColor}
-                                  updateHighlightedNode={this.updateHighlightedNode}
-                              />
-                      }
-                      )}
-                  </React.Fragment>
-              )}
+            {this.state.schematize.map(
+              (schematizeComponent, i)=> {
+                return (
+                    <React.Fragment>
+                      {/*These two lines could be in separate for loops if you want control over ordering*/}
+                      {this.renderComponent(schematizeComponent, i)}
+                      {this.allArrivalsForOneComponent(schematizeComponent, i)}
+                    </React.Fragment>
+                )
+            }
             )}
-            {this.state.schematize.map((schematizeComponent, i)=> {
-              return (
-                  <React.Fragment>
-                    {schematizeComponent.arrivals.map((linkColumn, j) => {
-                      const elevation = -15 - (j*this.state.binsPerPixel);
-                      const xOffsetGrid = (linkColumn.downstream + (i * this.state.paddingSize) + schematizeComponent.offset +
-                          (schematizeComponent.lastBin - schematizeComponent.firstBin + 1) + j) - .75;
-                      // const departureX = departure.offset + departure.arrivals.length;
-                      const localColor = stringToColourSave((linkColumn.downstream + 1) * (linkColumn.upstream + 1));
-                      let departureX = colToCoordMappingX[localColor][1];
-
-                      const arrowXCoord =  1 + this.state.leftOffset + xOffsetGrid*this.state.binsPerPixel;
-
-                      departureX =  departureX - arrowXCoord + 2;
-
-                        const departOrigin = [departureX, 0];
-                        const departCorner = [departureX - 2, elevation + 2];
-                        let departTop = [departureX-10, elevation];
-                        let arriveTop = [10, elevation];
-                        let arriveCorner = [1.5, elevation + 1.5]; // 1.5 in from actual corner
-                        const arriveCornerEnd = [0,-5];
-                        // we ran into a minimal sized arrow
-                      if (departureX <= 12) {
-                          departTop = [0, elevation + 2];
-                          arriveTop = [10 - 10 + 4, elevation + 2];
-                          arriveCorner = [0, elevation + 1.5]; // 1.5 in from actual corner
-                      }
-                      return <ArrowRect
-                            key={"arrow" + i+j}
-                            x={arrowXCoord}
-                            y={this.state.topOffset - 5}
-                            points={[
-                              departOrigin[0], departOrigin[1],
-                              departCorner[0], departCorner[1],
-                              departTop[0], departTop[1],
-                              arriveTop[0], arriveTop[1],
-                              arriveCorner[0], arriveCorner[1],
-                              arriveCornerEnd[0], arriveCornerEnd[1],
-                              0,0]}
-                            width={this.state.binsPerPixel}
-                            color={stringToColour((linkColumn.downstream + 1) * (linkColumn.upstream + 1), linkColumn, this.state.highlightedLinkId)}
-                        />})}
-                  </React.Fragment>
-              )
-            })}
           </Layer>
         </Stage>
       </React.Fragment>
     );
   }
+
 }
  
 render(<App />, document.getElementById('root'));
