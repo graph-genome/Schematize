265bb657fe286ab77a3852572516d68afb988280
BonfaceKilz <bonfacemunyoki@gmail.com>
2020-04-01 20:58:49 +0300

    style: remove unused variables after running `npm run lint`

--- src/PangenomeView.js
+++ src/PangenomeView.js
@@ -2,33 +2,18 @@ import App from './prototype/App'
 
 export default pluginManager => {
     const { jbrequire } = pluginManager
-    const { getRoot } = jbrequire('mobx-state-tree')
     const { observer, PropTypes } = jbrequire('mobx-react')
     const React = jbrequire('react')
-    const { useState } = jbrequire('react')
 
     // material-ui stuff
-    const Button = jbrequire('@material-ui/core/Button')
-    const Container = jbrequire('@material-ui/core/Container')
-    const FormControl = jbrequire('@material-ui/core/FormControl')
-    const FormGroup = jbrequire('@material-ui/core/FormGroup')
-    const FormLabel = jbrequire('@material-ui/core/FormLabel')
-    const Grid = jbrequire('@material-ui/core/Grid')
     const Icon = jbrequire('@material-ui/core/Icon')
     const IconButton = jbrequire('@material-ui/core/IconButton')
-    const MenuItem = jbrequire('@material-ui/core/MenuItem')
-    const Select = jbrequire('@material-ui/core/Select')
-    const ToggleButton = jbrequire('@material-ui/lab/ToggleButton')
     const { makeStyles } = jbrequire('@material-ui/core/styles')
     const { grey } = jbrequire('@material-ui/core/colors')
 
-    const { ResizeHandle } = jbrequire('@gmod/jbrowse-core/ui')
-    const { assembleLocString, getSession } = jbrequire('@gmod/jbrowse-core/util')
-    const { readConfObject } = jbrequire('@gmod/jbrowse-core/configuration')
-
     const useStyles = makeStyles(theme => {
         return {
-            root: {
+          root: {
                 position: 'relative',
                 marginBottom: theme.spacing(1),
                 overflow: 'auto',
