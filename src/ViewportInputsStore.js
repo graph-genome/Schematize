6bab14b98a97cd156cae44c7143fa0e7cdacdf49
6br <tfdvrt@gmail.com>
2020-04-08 13:45:54 +0900

    Reduce error messages due to type annotation

--- src/ViewportInputsStore.js
+++ src/ViewportInputsStore.js
@@ -19,7 +19,7 @@ RootStore = types
     highlightedLink: 0, // we will compare linkColumns
     maximumHeightThisFrame: 150,
     cellToolTipContent: "",
-    jsonName: "run1.B1phi1.i1.seqwish.w100",
+    jsonName: "nonconsecutive_chunks",
     startChunkURL: "",
     endChunkURL: "",
     pathNucPos: types.optional(PathNucPos, { path: "path", nucPos: 0 }), // OR: types.maybe(PathNucPos)
