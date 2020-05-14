9c670b848846bc2ff9c0fb61b489d319cf249709
mandosoft <thomasdtownsley@gmail.com>
2020-05-14 15:21:24 -0500

    Updates

---
+++ precache-manifest.890ea8a6746b88306fdc2b650c499bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedfd8ada22bfecc168c6061346fa389",
+    "url": "/Schematize/index.html"
+  },
+  {
+    "revision": "6806ba13de08fcfba2f6",
+    "url": "/Schematize/static/css/main.ea0bbd06.chunk.css"
+  },
+  {
+    "revision": "9a64c6518b67ac4a553d",
+    "url": "/Schematize/static/js/2.06453761.chunk.js"
+  },
+  {
+    "revision": "6806ba13de08fcfba2f6",
+    "url": "/Schematize/static/js/main.44a46dc8.chunk.js"
+  },
+  {
+    "revision": "a4e10835abd6053a24b1",
+    "url": "/Schematize/static/js/runtime-main.64ccc5b5.js"
+  }
+]);
